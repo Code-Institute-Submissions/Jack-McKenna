2925bcd8a9b26a363ded14b198f73481a70a29e9
Henry Curtis <hcuk106@gmail.com>
2017-12-14 12:11:07 +0000

    backup

--- js/app.js
+++ js/app.js
@@ -5,18 +5,14 @@ angular.module('JackApp').config(function($locationProvider, $routeProvider) {
  
     $routeProvider.when('/', {
         templateUrl: 'templates/home.html',
-        controller: 'HomeController'
     })
     .when('/about', {
         templateUrl: 'templates/about.html',
-        controller: 'AboutController'
     })
     .when('/portfolio', {
         templateUrl: 'templates/portfolio.html',
-        controller: 'PortfolioController'
     })
     .when('/contact', {
         templateUrl: 'templates/contact.html',
-        controller: 'ContactController'
     });
 });
--- js/elec.js
+++ js/elec.js
@@ -1,1 +1,95 @@
+// zoom to adapt to tablets
 document.body.style.zoom="100%"
+
+// image carousel
+var images =[
+    "images/Carousel-Images/electrician-1.jpg",
+    "images/Carousel-Images/electrician-2.jpg",
+    "images/Carousel-Images/electrician-3.jpg"
+]
+var num = 0
+
+function next() {
+    var slider = document.getElementById("slider");
+    num++;
+    if(num >= images.length) {
+        num = 0
+    }
+    slider.src = images[num];
+}
+
+function prev() {
+    var slider = document.getElementById("slider");
+    num--;
+    if(num < 0) {
+        num = images.length-1
+    }
+    slider.src = images[num];
+}
+
+// modals
+
+//modal variables
+var modal = document.getElementsByClassName('modal');
+var designModal = document.getElementById('modalDesign');
+//var installationModal = document.getElementById('installationModal')
+//var repairModal = document.getElementById('repairModal')
+
+//button variables
+var designBtn = document.getElementById('designBtn');
+//var installationBtn = document.getElementById('installationBtn')
+//var repairBtn = document.getElementById('repairBtn')
+
+//variables for closing
+var spanClose = document.getElementsByClassName('spanClose');
+var btnClose = document.getElementsByClassName('btnClose');
+
+
+
+window.onload = function() {
+    if(window.location.pathname.match(/\/portfolio/))
+    {
+        //click functions
+        designBtn.onclick = function() {
+            designModal.style.display = "block";
+        }
+
+        installationBtn.onclick = function() {
+            installationModal.style.display = "block";
+        };
+
+        repairBtn.onclick = function() {
+            repairModal.style.display = "block";
+        };
+
+        //close functions
+        spanClose.onclick = function () {
+            modal.style.display = "none";
+        };
+
+        btnClose.onclick = function () {
+            modal.style.display = "none";
+        };
+
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        };
+    }
+};
+
+// email submit JS
+$('#contact-Form').submit(function(e) {
+    var name = document.getElementById('usr')
+    var email = document.getElementById('email')
+    var text = document.getElementById('comment')
+
+    if (!name.value || !email.value || !text.value) {
+        alertify.error('Not all entries are valid. Please review and submit')
+    } else {
+        e.preventDefault()
+        $(this).get(0).reset()
+        alertify.success('Your message has been sent!')
+    }
+});
