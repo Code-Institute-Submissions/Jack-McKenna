235472c2b841afa31447b368c243be794052cf88
Henry Curtis <hcuk106@gmail.com>
2018-02-15 17:15:21 +0000

    removed email js

--- js/controller.js
+++ js/controller.js
@@ -60,17 +60,5 @@ angular.module('RouteControllers', [])
     
     .controller('ContactController', function($scope) {
 
-        $('#contact-Form').submit(function(e) {
-            var name = document.getElementById('usr')
-            var email = document.getElementById('email')
-            var text = document.getElementById('comment')
         
-            if (!name.value || !email.value || !text.value) {
-                alertify.error('Not all entries are valid. Please review and submit')
-            } else {
-                e.preventDefault()
-                $(this).get(0).reset()
-                alertify.success('Your message has been sent!')
-            }
-        });
     });
